b799da6dea4da7e920d7ebc3111a702a7cfa2f4d
Ubuntu <ubuntu@ip-172-31-3-103.ec2.internal>
2015-10-09 21:44:58 +0000

    Added commit and writeback delays of 1 cycle using queues.

--- simulator/src/simulator/ReorderBuffer.java
+++ simulator/src/simulator/ReorderBuffer.java
@@ -4,6 +4,7 @@
  */
 package simulator;
 
+import java.util.*;
 
 public class ReorderBuffer {
     
@@ -19,13 +20,19 @@ public class ReorderBuffer {
         }
     }
     
-    private int NR, NC, head;
+    private int NR, NC, head, writtenThisCycle;
     private ROBEntry[] ROB;
     private RegisterFile<Integer> intRegisters;
     private RegisterFile<Double> floatRegisters;
     private ReservationStations resStations;
     private Scoreboard scoreboard;
-    
+	private ArrayList<Integer> intIndexWriteQueue;
+	private ArrayList<Integer> floatIndexWriteQueue;
+	private ArrayList<Integer> intValueWriteQueue;
+    private ArrayList<Double> floatValueWriteQueue;
+	private ArrayList<Integer> indexCommitQueue;
+	private ArrayList<ROBEntry> entryCommitQueue;
+	
     public ReorderBuffer(int NR, int NC,  RegisterFile<Integer> intRegisters,  RegisterFile<Double> floatRegisters, ReservationStations resStations, Scoreboard scoreboard){
         this.NR = NR;
         this.NC = NC;
@@ -35,30 +42,75 @@ public class ReorderBuffer {
         this.scoreboard = scoreboard;
         head = 0;
         ROB = new ROBEntry[NR];
+		
+		writtenThisCycle = 0;
+		intIndexWriteQueue = new ArrayList<Integer>();
+		floatIndexWriteQueue = new ArrayList<Integer>();
+		intValueWriteQueue = new ArrayList<Integer>();
+		floatValueWriteQueue = new ArrayList<Double>();
+		indexCommitQueue = new ArrayList<Integer>();
+		entryCommitQueue = new ArrayList<ROBEntry>();
     }
     
-    public void cycle(int numCommitted) {
-        int current = head;
-        ROBEntry entry = ROB[current];
+    public void cycle() {
         
-        // Find the oldest instruction
+		// Write back what came from the execution stage last cycle and empty the writeback queues
+		for(int i = 0; i < intIndexWriteQueue.size(); i++) {
+			int robSlot = intIndexWriteQueue.get(i);
+			ROB[robSlot].hasValue = true;
+            ROB[robSlot].intValue = intValueWriteQueue.get(i);
+			resStations.writeback(robSlot, ROB[robSlot].intValue);
+			scoreboard.writeback(robSlot);
+			writtenThisCycle++;
+		}
+		intIndexWriteQueue.clear();
+		intValueWriteQueue.clear();
+		for(int i = 0; i < floatIndexWriteQueue.size(); i++) {
+			int robSlot = floatIndexWriteQueue.get(i);
+			ROB[robSlot].hasValue = true;
+            ROB[robSlot].floatValue = floatValueWriteQueue.get(i);
+			resStations.writeback(robSlot, ROB[robSlot].floatValue);
+			scoreboard.writeback(robSlot);
+			writtenThisCycle++;
+		}
+        floatIndexWriteQueue.clear();
+		floatValueWriteQueue.clear();
+		
+		// Write back what was committed last cycle and empty the commit queues
+		for(int i = 0; i < indexCommitQueue.size(); i++) {
+			int current = indexCommitQueue.get(i);
+			ROBEntry entry = entryCommitQueue.get(i);
+			commit(current, entry);
+		}
+		indexCommitQueue.clear();
+		entryCommitQueue.clear();
+		
+		writtenThisCycle = 0;
+    }
+    
+	public void commit() {
+		int current = head;
+        ROBEntry entry = ROB[current];
+		
+		// Find the oldest instruction
         while(entry == null) {
             current = next(current);
             entry = ROB[current];
         }
-        
-        // Commit in order from oldest instructions, if values are ready, while bandwidth available on the CDB
-        while(entry.hasValue && numCommitted < NC) {
-            commit(current, entry);
+		
+		// Commit in order from oldest instructions, if values are ready, while bandwidth available on the CDB
+        while(entry.hasValue && writtenThisCycle < NC) {
+			indexCommitQueue.add(current);
+			entryCommitQueue.add(entry);
             ROB[current] = null;
-            numCommitted++;
+            writtenThisCycle++;
             
             // Go to next oldest entry
             current = next(current);
             entry = ROB[current];
         }
-    }
-    
+	}
+	
     private void commit(int robSlot, ROBEntry entry) {
         if(entry.intValue != null) {
             intRegisters.write(entry.inst.dest.registerIndex, entry.intValue);
@@ -86,18 +138,26 @@ public class ReorderBuffer {
         return index;
     }
     
-    public void write(int index, Integer val) {
-        if(index >= 0 && index < NR) {
-            ROB[index].hasValue = true;
-            ROB[index].intValue = val;
-        }
+    public boolean write(int index, Integer val) {
+        if(writtenThisCycle < NC && index >= 0 && index < NR) {
+			intIndexWriteQueue.add(index);
+			intValueWriteQueue.add(val);
+			writtenThisCycle++;
+			return true;
+        } else {
+			return false;
+		}
     }
     
-    public void write(int index, Double val) {
-        if(index >= 0 && index < NR) {
-            ROB[index].hasValue = true;
-            ROB[index].floatValue = val;
-        }
+    public boolean write(int index, Double val) {
+        if(writtenThisCycle < NC && index >= 0 && index < NR) {
+			floatIndexWriteQueue.add(index);
+			floatValueWriteQueue.add(val);
+			writtenThisCycle++;
+			return true;
+        } else {
+			return false;
+		}
     }
     
     private int next() {
