a598f93ca2093aa95fe93189e3fc3baf03e76dd4
zaeem <zaeem@MININT-OH6U60B>
2015-10-11 03:27:50 +0500

    Partial load store unit

---
+++ simulator/src/simulator/LoadStoreUnit.java
@@ -0,0 +1,51 @@
+package simulator;
+
+public class LoadStoreUnit {
+
+    private Unit LSU;
+    ReorderBuffer reorderBuffer;
+    ReservationStations reservationStations;
+    
+    public LoadStoreUnit(ReservationStations reservationStations, ReorderBuffer reorderBuffer){
+        this.reorderBuffer = reorderBuffer;
+        this.reservationStations = reservationStations;
+        this.LSU.curCycle = 0;
+        this.LSU.instruction = null;
+        this.LSU.isWaiting = false;
+    }
+    
+    private class Unit{
+        Instruction instruction;
+        int curCycle;
+        boolean isWaiting;
+    }
+    
+    public void cycle(){
+        int stNum;
+        if(LSU.curCycle == 0){
+            stNum = -1;
+            stNum = reservationStations.isReady("Load/Store");
+            if(stNum != -1){
+                    LSU.instruction = reservationStations.getInstruction(stNum);
+                    LSU.curCycle++;
+            }
+        }
+        else if(!LSU.isWaiting){
+            LSU.instruction.memoryAddress = LSU.instruction.src.intValue + LSU.instruction.target.intValue;
+            if(true/*Rob method to write memory address and value*/){
+                LSU.curCycle = 0;
+                reservationStations.finishedExecution(LSU.instruction.stNum);
+            }
+            else{
+                LSU.isWaiting = true;
+            }
+        }
+        else{
+            if(true/*Rob method to write memory address and value*/){
+                LSU.curCycle = 0;
+                LSU.isWaiting = false;
+                reservationStations.finishedExecution(LSU.instruction.stNum);
+            }
+        }
+    }
+}
